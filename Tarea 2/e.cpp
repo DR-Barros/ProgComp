6997a6319aa19c0f85241192678cefcb4895b2a8
Daniel Radrigán <danielradriganb@gmail.com>
2024-04-02 19:42:39 -0300

    FEAT(T2c): se logra pasar el problema c

--- Tarea 2/e.cpp
+++ Tarea 2/e.cpp
@@ -2,55 +2,7 @@
 using namespace std;
 
 
-/* int main() {
-    int t;
-    cin >> t;
-    for (int i = 0; i < t; i++){
-        int n;
-        cin >> n;
-        //set <pair<int, int>> s;
-        set <pair<int, int>> s;
-        for (int i = 0; i < n; i++){
-            int x;
-            cin >> x;
-            s.insert({x, i});
-        }
-        int res = 0;
-        // (max1 + max2 + max3) - (j - i)
-        while (s.size() > 2){
-            auto it = s.end();
-            it--;
-            int max1 = it->first;
-            int idx1 = it->second;
-            it--;
-            int max2 = it->first;
-            int idx2 = it->second;
-            it--;
-            int max3 = it->first;
-            int idx3 = it->second;
-            if (idx1 < idx2){
-                swap(max1, max2);
-                swap(idx1, idx2);
-            }
-            if (idx2 < idx3){
-                swap(max2, max3);
-                swap(idx2, idx3);
-            }
-            if (idx1 < idx2){
-                swap(max1, max2);
-                swap(idx1, idx2);
-            }
-            res = max(res, max1 + max2 + max3 - idx1);
-            s.erase({max1, idx1});
-        }
-
-        cout << res << endl;
-    }   
-    
-}
- */
-
-/* int main() {
+int main(){
     int t;
     cin >> t;
     for (int i = 0; i < t; i++){
@@ -60,67 +12,52 @@ using namespace std;
         for (int j = 0; j < n; j++){
             cin >> a[j];
         }
-        int res = 0;
-        // calcular el maximo del subarreglo de tamaño arbitrario con los 3 elementos mas grandes - tamaño del subarreglo
-        for (int j = 0; j < n; j++){
-            int max1 = 0;
-            int max2 = 0;
-            int max3 = 0;
-            for (int k = j; k < n; k++){
-                if (a[k] > max1){
-                    max3 = max2;
-                    max2 = max1;
-                    max1 = a[k];
-                } else if (a[k] > max2){
-                    max3 = max2;
-                    max2 = a[k];
-                } else if (a[k] > max3){
-                    max3 = a[k];
-                }
-                res = max(res, max1 + max2 + max3 - (k - j));
-            }
-        }
-
-        cout << res << endl;
-    }   
-    
-}
- */
-
-/* int main() {
-    int t;
-    cin >> t;
-    for (int i = 0; i < t; i++){
-        int n;
-        cin >> n;
-        vector<int> a(n);
-        for (int j = 0; j < n; j++){
-            cin >> a[j];
-        }
-        int res = 0;
+        // maximo entre (a[j] + a[k] + a[l] - (l -r)) donde r y l son los indices extremos
         int max1 = 0;
         int max2 = 0;
         int max3 = 0;
-        int first = 0;
-        // calcular el maximo del subarreglo de tamaño arbitrario con los 3 elementos mas grandes - tamaño del subarreglo
+        int pos1 = 0;
+        int pos2 = 0;
+        int pos3 = 0;
+        // encontramos el maximo de mas a la izquierda
         for (int j = 0; j < n; j++){
             if (a[j] > max1){
                 max3 = max2;
+                pos3 = pos2;
                 max2 = max1;
+                pos2 = pos1;
                 max1 = a[j];
+                pos1 = j;
             } else if (a[j] > max2){
                 max3 = max2;
+                pos3 = pos2;
                 max2 = a[j];
+                pos2 = j;
             } else if (a[j] > max3){
                 max3 = a[j];
-            }
-            res = max(res, (max1 + max2 + max3) - (j - first));
-            if (j - first > max1 + max2 + max3){
-                first++;
+                pos3 = j;
             }
         }
-
+        int l = max(pos1, max(pos2, pos3));
+        int value = a[l];
+        //recorremos hacia la izquierda buscando el maximo
+        int max4 = 0;
+        int pos4 = l;
+        int max5 = 0;
+        int pos5 = l;
+        int res = value;
+        for (int j = l-1; j >= 0; j--){ 
+            if (a[j] > max4){
+                max5 = max4;
+                pos5 = pos4;
+                max4 = a[j];
+                pos4 = j;
+            } else if (a[j] > max5){
+                max5 = a[j];
+                pos5 = j;
+            }
+            res = max(res, value + max4 + max5 - (l - j));
+        }
         cout << res << endl;
-    }   
-    
-} */
+    }
+}
