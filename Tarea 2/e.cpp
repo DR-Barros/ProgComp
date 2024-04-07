26e57f79f2dac224b04c337dab31b10446bb3fef
Daniel Radrigán <danielradriganb@gmail.com>
2024-04-07 13:12:20 -0400

    FEAT(T2e): se implementa una solucion para e

--- Tarea 2/e.cpp
+++ Tarea 2/e.cpp
@@ -8,56 +8,30 @@ int main(){
     for (int i = 0; i < t; i++){
         int n;
         cin >> n;
-        vector<int> a(n);
-        for (int j = 0; j < n; j++){
-            cin >> a[j];
+        vector<int> a(n); 
+        for (int i = 0; i < n; i++){
+            int num1;
+            cin >> num1;
+            a[i] = num1;
         }
-        // maximo entre (a[j] + a[k] + a[l] - (l -r)) donde r y l son los indices extremos
-        int max1 = 0;
-        int max2 = 0;
-        int max3 = 0;
-        int pos1 = 0;
-        int pos2 = 0;
-        int pos3 = 0;
-        // encontramos el maximo de mas a la izquierda
-        for (int j = 0; j < n; j++){
-            if (a[j] > max1){
-                max3 = max2;
-                pos3 = pos2;
-                max2 = max1;
-                pos2 = pos1;
-                max1 = a[j];
-                pos1 = j;
-            } else if (a[j] > max2){
-                max3 = max2;
-                pos3 = pos2;
-                max2 = a[j];
-                pos2 = j;
-            } else if (a[j] > max3){
-                max3 = a[j];
-                pos3 = j;
-            }
+        // b corresponde al máximo valor de a[i] + i desde la izquierda (bi1 - l)
+        // c corresponde al máximo valor de a[i] - i desde la derecha (bi3 -r)
+        vector<int> b(n), c(n);
+        for (int i = 0; i < n; i++){
+            b[i] = a[i]+i;
+            c[i] = a[i]-i;
         }
-        int l = max(pos1, max(pos2, pos3));
-        int value = a[l];
-        //recorremos hacia la izquierda buscando el maximo
-        int max4 = 0;
-        int pos4 = l;
-        int max5 = 0;
-        int pos5 = l;
-        int res = value;
-        for (int j = l-1; j >= 0; j--){ 
-            if (a[j] > max4){
-                max5 = max4;
-                pos5 = pos4;
-                max4 = a[j];
-                pos4 = j;
-            } else if (a[j] > max5){
-                max5 = a[j];
-                pos5 = j;
-            }
-            res = max(res, value + max4 + max5 - (l - j));
+        for (int i = 1; i < n; i++){
+            b[i] = max(b[i], b[i-1]);
         }
-        cout << res << endl;
+        for (int i = n-2; i >= 0; i--){
+            c[i] = max(c[i], c[i+1]);
+        }
+
+        int ans = 0;
+        for (int i = 1; i < n-1; i++){
+            ans = max(ans, a[i]+b[i-1]+c[i+1]); // a[i] = bi2, luego ans = bi2 + bi1 + bi3 - (r-l)
+        }
+        cout << ans << endl;
     }
 }
